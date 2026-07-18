5fa82d80ed47317e55ef20314ec1e92d47f74912
sairamv218 <sairamv218@gmail.com>
2026-07-19 00:02:00 +0530

    comments cron

--- src/utils/cronjob.js
+++ src/utils/cronjob.js
@@ -1,6 +1,10 @@
 const cron = require('node-cron');
 // CRONTAB FOR REFERENCE
 
+// * * * * * which means you are excluding seconds
+//* * * * * * this will trigger on evry second 
+// User crontab for the exact expression
+
 cron.schedule("* * * * *",()=>{
     try{
  // BULL MQ TO HANDLE THE MULTIPLE CRONS IN BACTHES
